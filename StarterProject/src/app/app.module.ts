fe5f608e26d72267213df00f9550a0bc72c4f3d5
hamed <naeemaei@outlook.com>
2022-01-04 16:52:57 +0330

    Add Starter Project

---
+++ StarterProject/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
