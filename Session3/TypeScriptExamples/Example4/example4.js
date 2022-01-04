743766229458c050c2c62b8c9d1e91685acda461
hamed <naeemaei@outlook.com>
2022-01-05 00:18:36 +0330

    Add ts transpile to js any example

---
+++ Session3/TypeScriptExamples/Example4/example4.js
@@ -0,0 +1,5 @@
+var first;
+first = 12;
+console.log(first);
+first = 'Ali';
+console.log(first);
---
+++ Session3/TypeScriptExamples/Example4/example4.ts
@@ -0,0 +1,5 @@
+var first: any;
+first = 12;
+console.log(first);
+first = 'Ali'
+console.log(first);
