d060c0f5175fa23cabed8502d99542219668ca54
hamed <naeemaei@outlook.com>
2022-01-05 00:18:15 +0330

    Add ts transpile to js trim example

---
+++ Session3/TypeScriptExamples/Example2/example2.js
@@ -0,0 +1,6 @@
+var num1 = 12;
+var text = '        Hello World!      ';
+var num2 = 12;
+console.log(num2.trim());
+console.log(text.trim());
+console.log(num1);
---
+++ Session3/TypeScriptExamples/Example2/example2.ts
@@ -0,0 +1,8 @@
+var num1: number = 12;
+var text: string = '        Hello World!      ';
+var num2: number = 12;
+
+console.log(num2.trim());
+console.log(text.trim());
+
+console.log(num1);
